30c5f5af171389f62703d8089b702ae5172a0864
wqyjason <qingyuw2@illinois.edu>
2019-10-04 21:38:24 -0500

    fix

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def hello():
+
     f = open("data.txt", "r")
     reader = f.readlines()
     for i in reader:
@@ -12,8 +13,15 @@ def hello():
             num = i.split(' ')[1]
         elif 'air' in i:
             air = i.split(' ')[1]
-    return render_template("index.html", num=num, air=air)
+        elif 'predict' in i:
+            predict = i.split(' ')[1]
+
+    return render_template("index.html", num=num, air=air, predict=predict)
+
+
 
 
 if __name__ == '__main__':
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(debug = True)
